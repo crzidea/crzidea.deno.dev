73049456e5c3f9a97ef1380399fecdc521b96d26
Crzidea <i@crzidea.com>
2023-06-20 19:15:16 +0800

    Add rateOfReturn and remove unused code

--- routes/which-day-should-i-buy-in-000001-SS.ts
+++ routes/which-day-should-i-buy-in-000001-SS.ts
@@ -13,47 +13,37 @@ router.get("/", async (ctx) => {
     [key: number]: {
       date: Date;
       close: number;
-      open: number;
-      high: number;
-      low: number;
-      volume: number;
     }[];
   } = {};
   for (let i = 0; i < result.timestamp.length; i++) {
     const date = new Date(result.timestamp[i] * 1000);
     const day = date.getDay();
     const close = result.indicators.quote[0].close[i];
-    const open = result.indicators.quote[0].open[i];
-    const high = result.indicators.quote[0].high[i];
-    const low = result.indicators.quote[0].low[i];
-    const volume = result.indicators.quote[0].volume[i];
     pricesOfDays[day] = pricesOfDays[day] || [];
     pricesOfDays[day].push({
       date,
       close,
-      open,
-      high,
-      low,
-      volume,
     });
   }
   const summaryOfDays: {
     [key: number]: {
       averageClosePrice: number;
-      tp90ClosePrice: number;
+      rateOfReturn: number;
       days: number;
     };
   } = {};
+  const latestPrice = result.indicators.quote[0].close.pop();
   for (const day in pricesOfDays) {
     const prices = pricesOfDays[day];
     const averageClosePrice =
       prices.reduce((sum, price) => sum + price.close, 0) / prices.length;
-    const tp90ClosePrice = prices.sort((a, b) => b.close - a.close)[
-      Math.floor(prices.length * 0.1)
-    ].close;
+    // Compute how much money you would have made if you bought in on each day
+    // and sold on the last day.
+    const rateOfReturn = latestPrice / averageClosePrice - 1;
+
     summaryOfDays[day] = {
       averageClosePrice,
-      tp90ClosePrice,
+      rateOfReturn,
       days: prices.length,
     };
   }
